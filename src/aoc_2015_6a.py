03bdfeeffe4f98615429b3ca32f449335f2900e6
Roald87 <roaldruiter@gmail.com>
2021-02-01 20:53:42 +0100

    Added roald1.

--- src/aoc_2015_6a.py
+++ src/aoc_2015_6a.py
@@ -11,9 +11,10 @@ After following the instructions, how many lights are lit?
 import numpy as np
 import re
 
+
 def tom1(instructions: list):
     def line_parser(line):
-        expr = '(\w+) (\d+),(\d+) through (\d+),(\d+)'
+        expr = "(\w+) (\d+),(\d+) through (\d+),(\d+)"
         action, x1, y1, x2, y2 = re.search(expr, line).groups()
         return action, int(x1), int(y1), int(x2), int(y2)
 
@@ -21,12 +22,38 @@ def tom1(instructions: list):
     grid_pt1 = np.zeros((1000, 1000)) - 1  # start at -1 to indicate off
     for line in instructions:
         action, x1, y1, x2, y2 = line_parser(line)
-        if action == 'on':
-            grid_pt1[x1:x2 + 1, y1:y2 + 1] = 1
+        if action == "on":
+            grid_pt1[x1 : x2 + 1, y1 : y2 + 1] = 1
 
-        elif action == 'off':
-            grid_pt1[x1:x2 + 1, y1:y2 + 1] = -1
+        elif action == "off":
+            grid_pt1[x1 : x2 + 1, y1 : y2 + 1] = -1
 
-        elif action == 'toggle':
-            grid_pt1[x1:x2 + 1, y1:y2 + 1] = -1 * grid_pt1[x1:x2 + 1, y1:y2 + 1]
+        elif action == "toggle":
+            grid_pt1[x1 : x2 + 1, y1 : y2 + 1] = -1 * grid_pt1[x1 : x2 + 1, y1 : y2 + 1]
     return np.sum(grid_pt1[grid_pt1 == 1])
+
+
+def roald1(instructions: list):
+    def line_parser(line):
+        split_line = line.split()
+        x1, y1 = split_line[-3].split(",")
+        x2, y2 = split_line[-1].split(",")
+
+        if "toggle" in line:
+            action = -1
+        elif "off" in line:
+            action = 0
+        else:
+            action = 1
+
+        return action, int(x1), int(y1), int(x2), int(y2)
+
+    grid = np.zeros((1000, 1000), dtype=bool)
+    for line in instructions:
+        action, x1, y1, x2, y2 = line_parser(line)
+        if action >= 0:
+            grid[x1 : x2 + 1, y1 : y2 + 1] = bool(action)
+        else:
+            grid[x1 : x2 + 1, y1 : y2 + 1] = np.invert(grid[x1 : x2 + 1, y1 : y2 + 1])
+
+    return np.sum(grid)
