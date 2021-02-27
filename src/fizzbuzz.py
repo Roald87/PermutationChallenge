abcd3f3948b02956fe0bca451bc625c46dfbf109
Roald87 <roaldruiter@gmail.com>
2021-02-27 12:17:48 +0100

    Added trivial case.

--- src/fizzbuzz.py
+++ src/fizzbuzz.py
@@ -223,3 +223,10 @@ def roald8():
                 yield str(i)
 
     return " ".join(i for i in fizzbuzz_gen())
+
+
+def roald9():
+    return (
+        "1 2 Fizz 4 Buzz Fizz 7 8 Fizz Buzz 11 Fizz 13 14 FizzBuzz 16 17 "
+        + "Fizz 19 Buzz Fizz 22 23 Fizz Buzz 26 Fizz 28 29 FizzBuzz"
+    )
